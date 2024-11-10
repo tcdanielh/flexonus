f96c6c004bd20b588a9563761b867eb31af4a516
Seong Hyun Park <spark6015@berkeley.edu>
2024-11-10 00:50:22 -0800

    wall

--- Assets/Fracture.cs
+++ Assets/Fracture.cs
@@ -1,24 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class Fracture : MonoBehaviour
+public class Fracture : NetworkBehaviour
 {
     public GameObject fracturedObject;
     void OnCollisionEnter(Collision collision)
     {
         if (collision.transform.CompareTag("attack"))
         {
-            fracturedObject = Instantiate(fracturedObject, transform.position, transform.rotation);
             gameObject.GetComponent<MeshRenderer>().enabled = false;
+            fracturedObject.SetActive(true);
+            ShatterObject(fracturedObject);
             StartCoroutine(Disappear());
         }
     }
 
+    void ShatterObject(GameObject fracturedObject)
+    {
+        fracturedObject.SetActive(true);
+
+        // Apply forces to each piece of the fractured object
+        foreach (Transform piece in fracturedObject.transform)
+        {
+            Rigidbody rb = piece.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                // Apply a random explosion force for a shattering effect
+                Vector3 explosionDir = (piece.transform.position - fracturedObject.transform.position).normalized;
+                float explosionForce = Random.Range(100f, 500f);
+                rb.AddForce(explosionDir * explosionForce);
+            }
+        }
+    }
+
+
     IEnumerator Disappear()
     {
-        yield return new WaitForSeconds(3f);
-        Destroy(fracturedObject);
+        yield return new WaitForSeconds(4f);
         Destroy(gameObject);
     }
 }
